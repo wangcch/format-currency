85f9af52972d556fe64d71f2c31ab266bf33582c
wangcch <ci713@qq.com>
2018-11-01 02:50:54 +0800

    update js and test

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const defaultOptions = {
-  segmentDigit: 3
+  decimalDigit: 2,
+  segmentDigit: 3,
+  isSegment: true
 };
 
-const formatCurrency = str => {
-  const reg = new RegExp(
-    `\\B(?=(\\d{${defaultOptions.segmentDigit}})+(?!\\d))`,
-    "g"
+const NUMBER = "number";
+const STRING = "string";
+
+const formatCurrency = (value, options) => {
+  const { decimalDigit, segmentDigit, isSegment } = Object.assign(
+    {},
+    defaultOptions,
+    options
   );
-  return str.replace(reg, ",");
+  const currency = isNumber(value) ? value.toString() : value;
+  if (isCurrencyString(currency)) {
+    // TODO
+    return formatAddComma(currency, segmentDigit);
+  } else {
+    throw new TypeError("type error");
+    return NaN;
+  }
 };
 
-module.exports = formatCurrency;
+const parseCurrency = currency => {
+  return parseInt(currency.replace(/\$\s?|(,*)/g, ""), 10);
+};
+
+const formatAddComma = (string, segmentDigit) => {
+  const reg = new RegExp(`\\B(?=(\\d{${segmentDigit}})+(?!\\d))(\.\d)?`, "g");
+  return string.replace(reg, ",");
+};
+
+const isNumber = value => {
+  if (!isNaN(value) && typeof value === NUMBER) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const isCurrencyString = value => {
+  if (typeof value === STRING && value.length > 0) {
+    // TODO
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = { formatCurrency, parseCurrency };
--- test.js
+++ test.js
@@ -1,8 +1,31 @@
-import test from 'ava';
-import formatCurrency from './index.js';
+import test from "ava";
+import { formatCurrency, parseCurrency } from "./index.js";
 
-test('test', t => {
-    t.is(formatCurrency('123'), '123');
-    t.is(formatCurrency('123456'), '123,456');
-    t.is(formatCurrency('1234567'), '1,234,567');
+test("format-integer", t => {
+  t.is(formatCurrency("123"), "123");
+  t.is(formatCurrency("123456"), "123,456");
+  t.is(formatCurrency("1234567"), "1,234,567");
+  t.is(formatCurrency("-123"), "-123");
+  t.is(formatCurrency("+123"), "+123");
 });
+
+test("format-decimal", t => {
+  t.is(formatCurrency("123.00"), "123.00");
+  // t.is(formatCurrency("123.0000"), "123.00");
+  // t.is(formatCurrency("1234.1234"), "1,234.12");
+  // t.is(formatCurrency("1234.125"), "1,234.13");
+});
+
+test("format-typeErr", t => {
+  t.is(formatCurrency(""), NaN);
+  t.is(formatCurrency({}), NaN);
+  // t.is(formatCurrency("abc"), NaN);
+})
+
+test("parse-integer", t => {
+  t.is(parseCurrency("123"), 123);
+  t.is(parseCurrency("123,456"), 123456);
+  t.is(parseCurrency("1,234,567"), 1234567);
+  t.is(parseCurrency("-123"), -123);
+  t.is(parseCurrency("+123"), +123);
+})
